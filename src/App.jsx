690031ac91a82e7ecc50d97d1b24a457354fa277
MarianoVergniaud <mariano.unla@gmail.com>
2025-12-09 10:46:26 -0300

    final

--- src/App.jsx
+++ src/App.jsx
@@ -161,12 +161,12 @@ function App() {
           © {new Date().getFullYear()}{" "}
           Desarrollado por{" "}
           <a
-            href="https://magozitsolutions.netlify.app/"
+            href="https://gopedidos-psi.vercel.app/#"
             target="_blank"
             rel="noopener noreferrer"
             className="text-decoration-none text-info"
           >
-            MagoZ IT Solutions
+            GoPedidos
           </a>
         </small>
       </footer>
